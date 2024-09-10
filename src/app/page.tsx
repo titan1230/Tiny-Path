947c4d680655944c55167f73885164e11fecf9fc
Akshat Jain <akshat00jain@gmail.com>
2024-09-10 23:42:23 +0530

    refactor: Update landing page layout and add smooth scrolling behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
 import React from "react";
-import Image from 'next/image'
+import Image from 'next/image';
+import { FiLink, FiEdit, FiBarChart2, FiLock } from 'react-icons/fi';
+import Link from "next/link";
 
 const LandingPage: React.FC = () => {
   return (
     <div className="bg-base-100 text-white">
       {/* Header */}
-      <header className="bg-neutral text-white py-4 px-8 flex justify-between items-center">
+      <header className="bg-neutral text-white py-4 px-8 flex justify-between items-center fixed top-0 w-full z-50">
         <div className="flex items-center">
           <Image src="/TinyPath.png" alt="TinyPath Logo" height={40} width={40} priority />
           <nav className="ml-8">
-            <a href="#home" className="text-white mx-4">Home</a>
-            <a href="#features" className="text-white mx-4">Features</a>
-            <a href="#pricing" className="text-white mx-4">Pricing</a>
-            <a href="#about" className="text-white mx-4">About Us</a>
+            <Link href="#home" className="text-white mx-4">Home</Link>
+            <Link href="#features" className="text-white mx-4">Features</Link>
+            <Link href="#pricing" className="text-white mx-4">Pricing</Link>
+            <Link href="#about" className="text-white mx-4">About Us</Link>
           </nav>
         </div>
-        <button className="bg-primary text-white py-2 px-4 rounded-lg hover:bg-[#1171ee] ease-in-out transition-all">
-          Get Started
-        </button>
+        <Link href="/dashboard">
+          <button className="bg-primary text-white py-2 px-4 rounded-lg hover:bg-[#1171ee] ease-in-out transition-all">
+            Get Started
+          </button>
+        </Link>
       </header>
 
       {/* Hero Section */}
-      <section id="home" className="hero bg-base-100 text-white py-20 flex flex-col items-center text-center min-h-screen justify-center">
+      <section id="home" className="hero bg-hero-bg text-white py-20 flex flex-col items-center text-center min-h-screen justify-center">
         <h1 className="text-5xl font-bold mb-4">Shorten, Share, and Track Your Links with Ease</h1>
         <p className="text-lg mb-8">TinyPath makes URL shortening simple and effective for everyone.</p>
         <button className="bg-primary text-white py-3 px-6 rounded-full hover:bg-[#1171ee] ease-in-out transition-all">
@@ -30,36 +34,36 @@ const LandingPage: React.FC = () => {
       </section>
 
       {/* Features Section */}
-      <section id="features" className="features bg-base-200 text-white py-20">
+      <section id="features" className="features bg-base-200 text-white py-32">
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-10">Why Choose TinyPath?</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            <div className="feature-item">
-              <i className="fas fa-link text-primary text-4xl mb-4"></i>
+            <div className="feature-item flex flex-col items-center">
+              <FiLink className="text-primary text-4xl mb-4" />
               <h3 className="text-xl font-semibold mb-2">Easy URL Shortening</h3>
-              <p>Shorten any URL in seconds with our user-friendly interface.</p>
+              <p className="text-center">Shorten any URL in seconds with our user-friendly interface.</p>
             </div>
-            <div className="feature-item">
-              <i className="fas fa-edit text-primary text-4xl mb-4"></i>
+            <div className="feature-item flex flex-col items-center">
+              <FiEdit className="text-primary text-4xl mb-4" />
               <h3 className="text-xl font-semibold mb-2">Customizable Links</h3>
-              <p>Create branded and customizable short links to enhance your brand identity.</p>
+              <p className="text-center">Create branded and customizable short links to enhance your brand identity.</p>
             </div>
-            <div className="feature-item">
-              <i className="fas fa-chart-line text-primary text-4xl mb-4"></i>
+            <div className="feature-item flex flex-col items-center">
+              <FiBarChart2 className="text-primary text-4xl mb-4" />
               <h3 className="text-xl font-semibold mb-2">Analytics Dashboard</h3>
-              <p>Track link performance with real-time analytics and insights.</p>
+              <p className="text-center">Track link performance with real-time analytics and insights.</p>
             </div>
-            <div className="feature-item">
-              <i className="fas fa-lock text-primary text-4xl mb-4"></i>
+            <div className="feature-item flex flex-col items-center">
+              <FiLock className="text-primary text-4xl mb-4" />
               <h3 className="text-xl font-semibold mb-2">Secure and Reliable</h3>
-              <p>Enjoy secure and reliable service with top-notch privacy features.</p>
+              <p className="text-center">Enjoy secure and reliable service with top-notch privacy features.</p>
             </div>
           </div>
         </div>
       </section>
 
       {/* Pricing Section */}
-      <section id="pricing" className="pricing bg-base-100 text-white py-20">
+      <section id="pricing" className="pricing bg-base-100 text-white py-20 px-5">
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-10">Choose Your Plan</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
