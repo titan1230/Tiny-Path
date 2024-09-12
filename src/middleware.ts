bf812842cfdc3ee4e984a03296c27efb05809417
Akshat Jain <akshat00jain@gmail.com>
2024-09-12 21:10:23 +0530

    refactor: Update middleware to exclude SVG and PNG files from authentication check

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,13 @@ import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\.svg|.*\.png).*)"]
+}
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   const reqUrl = new URL(req.url);
-
-  if (!req.auth && reqUrl.pathname !== "/note/new" && reqUrl?.pathname.startsWith("/note/")) return;
-  if (!req.auth && reqUrl.pathname.startsWith("/user/")) return;
 
   if (!req.auth && reqUrl?.pathname !== "/") {
     if (reqUrl.pathname === "/onboarding") return;
@@ -20,7 +17,6 @@ export default auth((req) => {
     return NextResponse.redirect(new URL(`/onboarding`, req.url));
   }
 
-  // Redirect authenticated users from onboarding to dashboard
   if (req.auth && reqUrl?.pathname === "/onboarding") {
     return NextResponse.redirect(new URL(`/dashboard`, req.url));
   }
