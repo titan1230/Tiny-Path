c7f6e8d959beaba81cebeecc4587c02e9b11a957
Akshat Jain <akshat00jain@gmail.com>
2024-09-20 22:41:54 +0530

    Refactor OnboardingPage component: Add redirect functionality to Google login button

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -7,16 +7,21 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { signIn } from '@/lib/LoginUtils';
 
+import { useSearchParams } from 'next/navigation'
+
 const OnboardingPage: React.FC = () => {
+
+  const searchParams = useSearchParams()
+  const to = searchParams.get('to')
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-base-100 relative">
-      
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br to-[#5fd9ca] from-[#000025] relative">
+
       <Link href="/" className="absolute top-4 left-4">
         <FiArrowLeft className="text-2xl text-gray-600 hover:text-gray-800 cursor-pointer" />
       </Link>
 
-      <div className="bg-white shadow-lg rounded-lg p-8 md:w-1/3 w-full mx-4 flex flex-col items-center">
-        
+      <div className="bg-white/30 shadow-lg rounded-lg p-8 md:w-1/3 w-full mx-4 flex flex-col items-center backdrop-blur-lg backdrop-saturate-150 border border-white/20">
         <div className="mb-8">
           <Image src="/TinyPath.png" alt="Logo" width={80} height={80} className="mx-auto" />
         </div>
@@ -27,12 +32,13 @@ const OnboardingPage: React.FC = () => {
         <button
           type='button'
           className="bg-green-500 text-white py-3 px-4 w-full rounded-lg flex items-center justify-center gap-3 hover:bg-green-600 transition ease-in-out mt-8"
-          onClick={async () => await signIn()}
+          onClick={async () => await signIn(to || "/dashboard")}
         >
           <FcGoogle className="text-2xl" />
           <span className="font-semibold">Login with Google</span>
         </button>
       </div>
+
     </div>
   );
 };
