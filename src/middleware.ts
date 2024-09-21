84a471a49e2f7f003530f5d6e4ab2519cea70cf6
Akshat Jain <akshat00jain@gmail.com>
2024-09-21 16:48:34 +0530

    Refactor middleware.ts: Update matcher to only include dashboard routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,9 @@ import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|plans|contact|.*\.svg|.*\.png|.*\.mp4|.*\.lottie).*)"]
+  matcher: ["/dashboard/:path*"]
 }
+
 
 const { auth } = NextAuth(authConfig);
 
