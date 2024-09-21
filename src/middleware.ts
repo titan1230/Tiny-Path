c93b89d2bf0234f3a8d29e6d61a1a25c1d679c6f
Akshat Jain <akshat00jain@gmail.com>
2024-09-21 12:11:03 +0530

    Refactor middleware.ts: Update matcher to exclude lottie files

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\.svg|.*\.png|.*\.mp4).*)"]
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|plans|.*\.svg|.*\.png|.*\.mp4|.*\.lottie).*)"]
 }
 
 const { auth } = NextAuth(authConfig);
