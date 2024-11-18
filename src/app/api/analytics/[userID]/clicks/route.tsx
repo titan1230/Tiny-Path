df967029458d1723e2df23a3f611e990e3a67856
Akshat Jain <akshat00jain@gmail.com>
2024-11-18 21:21:05 +0530

    Add API route for daily clicks analytics

---
+++ src/app/api/analytics/[userID]/clicks/route.tsx
@@ -0,0 +1,12 @@
+// app/api/analytics/clicks/route.js
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const dailyClicks = {
+    '01/10': 1194,
+    '02/10': 98,
+    '03/10': 150,
+  };
+
+  return NextResponse.json({ dailyClicks });
+}
