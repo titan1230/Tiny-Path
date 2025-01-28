da4fd4c7b20c0377400550a451961dbf3a9499d8
Akshat Jain <akshat00jain@gmail.com>
2025-01-29 01:35:38 +0530

    Add Redis configuration and rate limiting implementation

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,11 @@
 const config = {
     env: {
         databaseURL: process.env.AUTH_DRIZZLE_URL!,
+        mapBoxToken: process.env.MAP_BOX_TOKEN!,
+        upstash: {
+            redisUrl: process.env.UPSTASH_REDIS_URL!,
+            redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+        },
     },
 };
 
