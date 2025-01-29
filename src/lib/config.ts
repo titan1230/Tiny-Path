b4120f57d1a2900e87175832da2b25fe2622b91b
Akshat Jain <akshat00jain@gmail.com>
2025-01-29 16:09:24 +0530

    Update MapBox token environment variable to use NEXT_PUBLIC prefix

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 const config = {
     env: {
         databaseURL: process.env.AUTH_DRIZZLE_URL!,
-        mapBoxToken: process.env.MAP_BOX_TOKEN!,
+        mapBoxToken: process.env.NEXT_PUBLIC_MAP_BOX_TOKEN!,
         upstash: {
             redisUrl: process.env.UPSTASH_REDIS_URL!,
             redisToken: process.env.UPSTASH_REDIS_TOKEN!,
