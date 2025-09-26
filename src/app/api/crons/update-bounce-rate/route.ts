3f7759683976929b45706ef012129a0c16fdf4bd
Akshat Jain <akshat00jain@gmail.com>
2025-09-26 19:38:36 +0530

    Initialize GET endpoint for updating bounce rate in analytics

---
+++ src/app/api/crons/update-bounce-rate/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import { db } from "@/database/drizzle";
+import { analytics, urls } from "@/database/schema";
+import { sql } from "drizzle-orm";
+
+export async function GET() {
+    
+}
